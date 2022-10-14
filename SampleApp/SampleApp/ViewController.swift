dfc92d485633e9f3fbc9db374d92ed9b0f06ec03
Istvan <istvan.balogh@outlook.com>
2022-10-14 20:31:39 +0200

    Add sample project

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Istvan Balogh on 14.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleApp/SampleAppUITests/SampleAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SampleAppUITestsLaunchTests.swift
+//  SampleAppUITests
+//
+//  Created by Istvan Balogh on 14.10.22.
+//
+
+import XCTest
+
+final class SampleAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
