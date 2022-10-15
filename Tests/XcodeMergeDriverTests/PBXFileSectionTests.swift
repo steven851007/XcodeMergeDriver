7fea68bbec90a8e9e8e3b457b7d9c7f6107807a6
Istvan <istvan.balogh@outlook.com>
2022-10-15 18:29:06 +0200

    Add test_changeSet_returnsCorrectChangeWhenLineAddedReferenceType

--- Tests/XcodeMergeDriverTests/PBXFileSectionTests.swift
+++ Tests/XcodeMergeDriverTests/PBXFileSectionTests.swift
@@ -10,20 +10,99 @@ import XCTest
 
 final class PBXFileSectionTests: XCTestCase {
     
-    func test_changeSet_returnsCorrectChangeWhenLineAdded() throws {
-        let base = try PBXFileSection(content: baseFile, type: .build)
-        var current = try PBXFileSection(content: currentFile, type: .build)
-        let other = try PBXFileSection(content: otherFile, type: .build)
+    func test_changeSet_returnsCorrectChangeWhenLineAddedBuildType() throws {
+        let base = try PBXFileSection(content: baseBuildFile, type: .build)
+        var current = try PBXFileSection(content: currentBuildFile, type: .build)
+        let other = try PBXFileSection(content: otherBuildFile, type: .build)
         
         let otherChangeset = other.difference(from: base)
         try current.applying(otherChangeset)
         
-        XCTAssertEqual(current, try PBXFileSection(content: resolvedFile, type: .build))
+        XCTAssertEqual(current, try PBXFileSection(content: resolvedBuildFile, type: .build))
+    }
+    
+    func test_changeSet_returnsCorrectChangeWhenLineAddedReferenceType() throws {
+        let base = try PBXFileSection(content: baseReferenceFile, type: .reference)
+        var current = try PBXFileSection(content: currentReferenceFile, type: .reference)
+        let other = try PBXFileSection(content: otherReferenceFile, type: .reference)
+        
+        let otherChangeset = other.difference(from: base)
+        try current.applying(otherChangeset)
+        
+        XCTAssertEqual(current, try PBXFileSection(content: resolvedReferenceFile, type: .reference))
     }
     
 }
 
-private let baseFile = """
+private let resolvedReferenceFile = """
+        87BBC76C28F9E1AB00380008 /* SampleApp.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = SampleApp.app; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC76F28F9E1AB00380008 /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
+        87BBC77128F9E1AB00380008 /* SceneDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneDelegate.swift; sourceTree = "<group>"; };
+        87BBC77328F9E1AB00380008 /* ViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewController.swift; sourceTree = "<group>"; };
+        87BBC77628F9E1AB00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
+        87BBC77828F9E1AD00380008 /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
+        87BBC77B28F9E1AD00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
+        87BBC77D28F9E1AD00380008 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+        87BBC78228F9E1AD00380008 /* SampleAppTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC78628F9E1AD00380008 /* SampleAppTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppTests.swift; sourceTree = "<group>"; };
+        87BBC78C28F9E1AD00380008 /* SampleAppUITests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppUITests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC79028F9E1AD00380008 /* SampleAppUITests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITests.swift; sourceTree = "<group>"; };
+        87BBC79228F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITestsLaunchTests.swift; sourceTree = "<group>"; };
+        87BBC7AD28F9F7FA00380008 /* BViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BViewController.swift; sourceTree = "<group>"; };
+        87BBC7AB28F9F7E700380008 /* AViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AViewController.swift; sourceTree = "<group>"; };
+"""
+
+private let currentReferenceFile = """
+        87BBC76C28F9E1AB00380008 /* SampleApp.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = SampleApp.app; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC76F28F9E1AB00380008 /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
+        87BBC77128F9E1AB00380008 /* SceneDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneDelegate.swift; sourceTree = "<group>"; };
+        87BBC77328F9E1AB00380008 /* ViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewController.swift; sourceTree = "<group>"; };
+        87BBC77628F9E1AB00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
+        87BBC77828F9E1AD00380008 /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
+        87BBC77B28F9E1AD00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
+        87BBC77D28F9E1AD00380008 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+        87BBC78228F9E1AD00380008 /* SampleAppTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC78628F9E1AD00380008 /* SampleAppTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppTests.swift; sourceTree = "<group>"; };
+        87BBC78C28F9E1AD00380008 /* SampleAppUITests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppUITests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC79028F9E1AD00380008 /* SampleAppUITests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITests.swift; sourceTree = "<group>"; };
+        87BBC79228F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITestsLaunchTests.swift; sourceTree = "<group>"; };
+        87BBC7AB28F9F7E700380008 /* AViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AViewController.swift; sourceTree = "<group>"; };
+"""
+
+private let otherReferenceFile = """
+        87BBC76C28F9E1AB00380008 /* SampleApp.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = SampleApp.app; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC76F28F9E1AB00380008 /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
+        87BBC77128F9E1AB00380008 /* SceneDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneDelegate.swift; sourceTree = "<group>"; };
+        87BBC77328F9E1AB00380008 /* ViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewController.swift; sourceTree = "<group>"; };
+        87BBC77628F9E1AB00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
+        87BBC77828F9E1AD00380008 /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
+        87BBC77B28F9E1AD00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
+        87BBC77D28F9E1AD00380008 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+        87BBC78228F9E1AD00380008 /* SampleAppTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC78628F9E1AD00380008 /* SampleAppTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppTests.swift; sourceTree = "<group>"; };
+        87BBC78C28F9E1AD00380008 /* SampleAppUITests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppUITests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC79028F9E1AD00380008 /* SampleAppUITests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITests.swift; sourceTree = "<group>"; };
+        87BBC79228F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITestsLaunchTests.swift; sourceTree = "<group>"; };
+        87BBC7AD28F9F7FA00380008 /* BViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BViewController.swift; sourceTree = "<group>"; };
+"""
+
+private let baseReferenceFile = """
+        87BBC76C28F9E1AB00380008 /* SampleApp.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = SampleApp.app; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC76F28F9E1AB00380008 /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
+        87BBC77128F9E1AB00380008 /* SceneDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneDelegate.swift; sourceTree = "<group>"; };
+        87BBC77328F9E1AB00380008 /* ViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ViewController.swift; sourceTree = "<group>"; };
+        87BBC77628F9E1AB00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
+        87BBC77828F9E1AD00380008 /* Assets.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Assets.xcassets; sourceTree = "<group>"; };
+        87BBC77B28F9E1AD00380008 /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/LaunchScreen.storyboard; sourceTree = "<group>"; };
+        87BBC77D28F9E1AD00380008 /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
+        87BBC78228F9E1AD00380008 /* SampleAppTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC78628F9E1AD00380008 /* SampleAppTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppTests.swift; sourceTree = "<group>"; };
+        87BBC78C28F9E1AD00380008 /* SampleAppUITests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = SampleAppUITests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
+        87BBC79028F9E1AD00380008 /* SampleAppUITests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITests.swift; sourceTree = "<group>"; };
+        87BBC79228F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SampleAppUITestsLaunchTests.swift; sourceTree = "<group>"; };
+"""
+
+private let baseBuildFile = """
         87BBC77028F9E1AB00380008 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC76F28F9E1AB00380008 /* AppDelegate.swift */; };
         87BBC77228F9E1AB00380008 /* SceneDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77128F9E1AB00380008 /* SceneDelegate.swift */; };
         87BBC77428F9E1AB00380008 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77328F9E1AB00380008 /* ViewController.swift */; };
@@ -35,7 +114,7 @@ private let baseFile = """
         87BBC79328F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC79228F9E1AD00380008 /* SampleAppUITestsLaunchTests.swift */; };
 """
 
-private let currentFile = """
+private let currentBuildFile = """
         87BBC77028F9E1AB00380008 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC76F28F9E1AB00380008 /* AppDelegate.swift */; };
         87BBC77228F9E1AB00380008 /* SceneDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77128F9E1AB00380008 /* SceneDelegate.swift */; };
         87BBC77428F9E1AB00380008 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77328F9E1AB00380008 /* ViewController.swift */; };
@@ -48,7 +127,7 @@ private let currentFile = """
         87BBC7AC28F9F7E700380008 /* AViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC7AB28F9F7E700380008 /* AViewController.swift */; };
 """
 
-private let otherFile = """
+private let otherBuildFile = """
         87BBC77028F9E1AB00380008 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC76F28F9E1AB00380008 /* AppDelegate.swift */; };
         87BBC77228F9E1AB00380008 /* SceneDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77128F9E1AB00380008 /* SceneDelegate.swift */; };
         87BBC77428F9E1AB00380008 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77328F9E1AB00380008 /* ViewController.swift */; };
@@ -61,7 +140,7 @@ private let otherFile = """
         87BBC7AE28F9F7FA00380008 /* BViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC7AD28F9F7FA00380008 /* BViewController.swift */; };
 """
 
-private let resolvedFile = """
+private let resolvedBuildFile = """
         87BBC77028F9E1AB00380008 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC76F28F9E1AB00380008 /* AppDelegate.swift */; };
         87BBC77228F9E1AB00380008 /* SceneDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77128F9E1AB00380008 /* SceneDelegate.swift */; };
         87BBC77428F9E1AB00380008 /* ViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 87BBC77328F9E1AB00380008 /* ViewController.swift */; };
